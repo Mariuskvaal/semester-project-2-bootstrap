273ddad8fcba73c5f434b246d5f250cf9a29e809
Mariuskvaal <mariuskvaal1@gmail.com>
2023-12-18 17:11:26 +0100

    login and register page

--- Login/login.js
+++ Login/login.js
@@ -1,68 +1,31 @@
-const formEl = document.querySelector("#loginForm");
-const method = "post";
-const messageEL = document.querySelector("#message");
+async function loginUser(email, password) {
+    // Endpoint URL
+    const url = 'https://api.noroff.dev/api/v1/auction/auth/login';
 
-formEl.addEventListener("submit", async (event) => {
-  event.preventDefault();
+    // Form data
+    const formData = new FormData();
+    formData.append('email', email);
+    formData.append('password', password);
 
-  const form = event.target;
-  const formData = new FormData(form);
-  const profile = Object.fromEntries(formData.entries());
-  await login(profile);
-  //console.log (formData.get('name'));
-});
+    try {
+        // Make the POST request
+        const response = await fetch(url, {
+            method: 'POST',
+            body: formData
+        });
 
-async function login(profile) {
-  const loginURL = `https://api.noroff.dev/api/v1/auction/auth/login`;
-  const body = JSON.stringify(profile);
-
-  const response = await fetch(loginURL, {
-    headers: {
-      "Content-Type": "application/json",
-    },
-    method: 'POST', // Set the method to POST
-    body,
-  });
-
-  const result = await response.json();
-
-  if (response.ok) {
-    showMessage("Login success");
-
-    localStorage.setItem("token", result.accessToken);
-
-    localStorage.setItem(
-      "user",
-      JSON.stringify({
-        name: result.name,
-        email: result.email,
-        avatar: result.avatar,
-        banner: result.banner,
-      })
-    );
-
-    const user = JSON.parse(localStorage.getItem("user"));
-    const token = localStorage.getItem("token");
-
-    console.log(token);
-    console.log(user);
-
-    // Display countdown
-    let countdown = 5;
-    const countdownInterval = setInterval(() => {
-      showMessage(`Redirecting in ${countdown}`);
-      countdown--;
-
-      if (countdown < 0) {
-        clearInterval(countdownInterval);
-        window.location.href = "/posts/index.html"; // Replace with the URL of the other HTML page
-      }
-    }, 1000);
-  } else {
-    showMessage("Login Failed");
-  }
+        // Check if the request was successful
+        if (response.ok) {
+            console.log('Login successful');
+            // Handle successful login here (e.g., redirect to another page)
+        } else {
+            console.error('Login failed');
+            // Handle login failure here (e.g., show error message)
+        }
+    } catch (error) {
+        console.error('Error during login:', error);
+    }
 }
 
-function showMessage(message) {
-  messageEL.textContent = message;
-}
+// Example usage
+// loginUser('user@example.com', 'password123');
