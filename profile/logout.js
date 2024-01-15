b21016c9392d6576eed7a3c5dacfbf7b24f63064
Mariuskvaal <mariuskvaal1@gmail.com>
2024-01-15 21:06:52 +0100

    removing user when logout

--- profile/logout.js
+++ profile/logout.js
@@ -1,6 +1,7 @@
 function logout() {
     // Remove the user's token from localStorage
     localStorage.removeItem("accessToken");
+    localStorage.removeItem("user");
   
     // Redirect to the login page or home page after logout
     // Replace 'login.html' with the path to your login page or home page
